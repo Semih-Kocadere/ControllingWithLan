060cec284f97fdf304f4fa2d4479d611dbdb8f52
Semih Kocadere <semih.kocadere@gmail.com>
2024-07-11 14:03:38 +0300

    Finished

---
+++ LedClient.py
@@ -0,0 +1,54 @@
+from socket import *
+import json
+
+HOST = "192.168.1.115"
+PORT = 8080
+BUFSIZE = 1024
+server_address = (HOST,PORT)
+pin = 11
+
+def authenticate():
+    user = input("Enter your username: ")
+    passw = input("Enter your password: ")
+    # Data to be sent
+    data = {
+        "username": user,
+        "password": passw
+    }
+    # Convert data to JSON string and encode to bytes
+    json_data = json.dumps(data).encode()
+
+    client_socket.send(json_data)
+
+    # Receive response from the server
+    response = client_socket.recv(1024).decode()
+    print(f"Received: {response}")
+
+    # Process the response
+    response_json = json.loads(response)
+    if response_json['authenticated']:
+        print("Authentication successful!")
+        return True
+    else:
+        print("Authentication failed:", response_json['message'])
+        return False
+def loop():
+    while True:
+        cmd = input("Input ON or OFF:")
+        client_socket.send(cmd.encode())
+try:
+    # Create a TCP/IP socket
+    client_socket = socket(AF_INET,SOCK_STREAM)
+
+    # Connect the socket to the server
+    client_socket.connect(server_address)
+
+    if authenticate():
+        loop()
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    # Clean up the connection
+    if 'client_socket' in locals():
+        client_socket.close()
---
+++ LedServer.py
@@ -0,0 +1,81 @@
+from socket import *
+from time import ctime
+import RPi.GPIO as GPIO
+import json
+
+HOST = ""
+PORT = 8080
+BUFSIZE = 1024
+server_address = (HOST, PORT)
+Username = "paye"
+Password = "12345"
+# IPV4 ailesi ve TCP tipinde bir soket olusturur.
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(server_address)
+server_socket.listen(5)
+
+pin = 11
+
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+
+def Led():
+    while True:
+        data = client_socket.recv(BUFSIZE).decode("utf-8").strip()
+        if not data:
+            break
+        if data == "ON":
+            GPIO.output(pin, GPIO.LOW)
+            print("Led on")
+        elif data == "OFF":
+            GPIO.output(pin, GPIO.HIGH)
+            print("Led off")
+        else:
+            print("error!")
+    server_socket.close()
+
+
+# Define a function to handle client connections
+def handle_client_connection(client_socket):
+    # Receive data from the client
+    data = client_socket.recv(1024).decode()
+    try:
+        json_data = json.loads(data)
+        username = json_data.get('username')
+        password = json_data.get('password')
+
+        # Example password check
+        if username == Username and password == Password:
+            response = {'authenticated': True, 'message': 'Authentication successful!'}
+        else:
+            response = {'authenticated': False, 'message': 'Invalid authorization!'}
+
+        # Send response back to client
+        client_socket.sendall(json.dumps(response).encode())
+
+        if response['authenticated']:
+            print("Successfully connected.")
+            setup()
+            Led()
+
+
+    except json.JSONDecodeError:
+        response = {'authenticated': False, 'message': 'Invalid JSON format.'}
+        client_socket.sendall(json.dumps(response).encode())
+
+    finally:
+        # Clean up the connection
+        client_socket.close()
+
+
+while True:
+    # Wait for a connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+
+    # Handle client connection in a separate thread or function
+    handle_client_connection(client_socket)
